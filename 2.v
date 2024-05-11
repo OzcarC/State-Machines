7a2dcf5b027214712bcc51fb969d5ccc70455d91
OzcarC <olopez2015.ol@gmail.com>
2024-05-10 21:27:03 -0700

    added partB circuit

--- 2.v
+++ 2.v
@@ -1,5 +1,32 @@
 `timescale 1ns/1ps
 
-module counterDecoder(
-    input stop,
+module dFF(
+    input D, clk, reset,
+    output reg Q
 );
+    always @(posedge clk) begin
+            Q <=D;
+    end
+endmodule
+
+module counterDecoder(
+    input stop, clk,
+    output out, 
+    output [3:0] countOut
+);
+
+    reg [3:0] ps;
+    wire [3:0] ns, inc;
+
+    always @(posedge clk)
+        ps <= ns;
+
+    assign inc = ps+1;
+
+    assign ns = stop ? ps : (ps == 7) ? 0 :inc;
+
+    assign countOut = ps;
+    assign out = (ps == 2) ? 1:0;
+
+endmodule
+
